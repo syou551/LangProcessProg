280e20386385038e7f8d0b4a862ecc67c4d65f64
syou551 <hiroto.0108si+@gmail.com>
2024-10-30 17:34:24 +0900

    add statement parser and update .gitignore

--- pp/parse.h
+++ pp/parse.h
@@ -8,10 +8,21 @@ extern int parse_block();
 extern int parse_var_dec();
 extern int parse_sub_program();
 extern int parse_compound_statement();
+extern int parse_statement();
 extern int parse_variable_names();
 extern int parse_variable_name();
 extern int parse_type();
 extern int parse_standard_type();
 extern int parse_array_type();
+extern int parse_formal_parameters();
+
+extern int parse_assign_statement();
+extern int parse_if_statement();
+extern int parse_while_statement();
+extern int parse_break_statement();
+extern int parse_call_statement();
+extern int parse_return_statement();
+extern int parse_input_statement();
+extern int parse_output_statement();
 
 #endif
--- pp/parser.c
+++ pp/parser.c
@@ -1,13 +1,16 @@
 #include "parse.h"
 
+
+int token;
+
 int parse_program(){
-    int token;
     token = scan();
     if(token != TPROGRAM) return error("ERROR: program must beign with \"program\"");
     token = scan();
     if(token != TNAME) return error("ERROR: required program name");
     token = scan();
     if(token != TSEMI) return error("ERROR: excpect \";\" next to program name");
+    token = scan();
     if(parse_block() == S_ERROR) return S_ERROR;
     token = scan();
     if(token != TDOT) return error("ERRPR: expect \".\" end of program");
@@ -15,55 +18,110 @@ int parse_program(){
 }
 
 int parse_block(){
-    while(parse_var_dec() != S_ERROR || parse_sub_program() != S_ERROR){}
+    while(token == TVAR || token == TPROCEDURE){
+        if(token == TVAR) {
+            token = scan();
+            if(parse_var_dec() == S_ERROR) return S_ERROR;
+        }
+        else if(token == TPROCEDURE) {
+            token = scan();
+            if(parse_sub_program() == S_ERROR) return S_ERROR;
+            token = scan();
+        }
+    }
     if(parse_compound_statement() == S_ERROR) return S_ERROR;
     return 0;
 }
 
-int parse_var_dec(){
-    int token;
+//#region statement
+
+int parse_compound_statement(){
+    if(token != TBEGIN) return error("ERROR: expect \"begin\" next to \";\"");
     token = scan();
-    if(token != TVAR) return S_ERROR;
-    parse_variable_names();
+    if(parse_statement() == S_ERROR) return S_ERROR;
+    token = scan();
+    while(token == TSEMI){
+        token = scan();
+        if(parse_statement() == S_ERROR) return S_ERROR;
+    }
+    if(token != TEND) return error("ERROR: expect \"end\"");
+    return 0;
+}
+
+int parse_statement(){
+    if(token == TNAME) return parse_assign_statement();
+    else if(token == TIF) return parse_if_statement();
+    else if(token == TWHILE) return parse_while_statement();
+    else if(token == TBREAK) return parse_break_statement();
+    else if(token == TCALL) return parse_call_statement();
+    else if(token == TRETURN) return parse_return_statement();
+    else if(token == TBEGIN) return parse_compound_statement();
+    else if(token == TREAD || token == TREADLN) return parse_input_statement();
+    else if(token == TWRITE || token == TWRITELN) return parse_output_statement();
+    //empty statement
+    else return S_ERROR;
+}
+
+//#endregion
+
+//#region variable
+int parse_var_dec(){
+    if(parse_variable_names() == S_ERROR) return S_ERROR;
     token = scan();
     if(token != TCOLON) return error("ERROR: expect \":\" next to variable names");
-    parse_type();
+    token = scan();
+    if(parse_type() == S_ERROR) return S_ERROR;
+    token = scan();
+    if(token != TSEMI) return error("ERROR: expect \";\" next to type");
+    token = scan();
+    while(token == TNAME){
+        if(parse_variable_names()==S_ERROR) return S_ERROR;
+        token = scan();
+        if(token != TCOLON) return error("ERROR: expect \":\" next to variable names");
+        token = scan();
+        if(parse_type() == S_ERROR) return S_ERROR;
+        token = scan();
+        if(token != TSEMI) return error("ERROR: expect \";\" next to type");
+        token = scan();
+    }
     return 0;
 }
 
 int parse_variable_names(){
-    int token;
     if(parse_variable_name() == S_ERROR) return S_ERROR;
-    while((token = scan()) == TSEMI){
+    token = scan();
+    while(token == TSEMI){
+        token = scan();
         if(parse_variable_name() == S_ERROR) return S_ERROR;
+        token = scan();
     }
     return 0;
 }
 
 int parse_variable_name(){
-    int token = scan();
-    if(token != TNAME) return error("ERROR: invarid variable declare");
+    if(token != TNAME) return error("ERROR: invarid variable name declare");
     return 0;
 }
 
 int parse_type(){
-    int token;
-    if(parse_standard_type() == S_ERROR && parse_array_type() == S_ERROR)return error("ERROR:invarid type declare");
+    if(token == TINTEGER || token == TBOOLEAN || token == TCHAR || token == TARRAY){ 
+        if(token == TARRAY) {
+            token = scan();
+            if(parse_array_type() == S_ERROR) return S_ERROR;
+        }else if(token == TINTEGER || token == TBOOLEAN || token == TCHAR) {
+            token = scan();
+            if(parse_standard_type() == S_ERROR) return S_ERROR;
+        }
+    }else error("ERROR: invarid type declare(expect integer, boolean, char or array)");
     return 0;
 }
 
 int parse_standard_type(){
-    int token;
-    token = scan();
     if(token != TINTEGER && token!= TBOOLEAN && token!= TCHAR) return S_ERROR;
     return 0;
 }
 
 int parse_array_type(){
-    int token;
-    token = scan();
-    if(token != TARRAY) return S_ERROR;
-    token = scan();
     if(token != TLSQPAREN) return error("ERROR: expect \"[\" next to \"array\"");
     token = scan();
     if(token != TNUMBER) return error("ERROR: invarid number input(array declare)");
@@ -71,6 +129,53 @@ int parse_array_type(){
     if(token != TRSQPAREN) return error("ERROR: expect \"]\" next to number");
     token = scan();
     if(token != TOF) return error("ERROR: expect \"of\" next to \"]\"");
+    token = scan();
     if(parse_standard_type() == S_ERROR)return S_ERROR;
     return 0;
 }
+
+//#endregion
+
+//#region sub program
+int parse_sub_program(){
+    if(token != TNAME) return error("ERROR: expect procedure name");
+    token = scan();
+    if(token == TLPAREN) {
+        token = scan();
+        if(parse_formal_parameters() == S_ERROR) return S_ERROR;
+    }
+    token = scan();
+    if(token != TSEMI) return error("ERROR: expect \";\" next to procedure name");
+    token = scan();
+    if(token == TVAR) {
+        token = scan();
+        if(parse_var_dec() == S_ERROR) return S_ERROR;
+    }
+    token = scan();
+    if(parse_compound_statement() == S_ERROR) return S_ERROR;
+    token = scan();
+    if(token != TSEMI) return error("ERROR: expect \";\" next to compound statement");
+    return 0;
+}
+
+int parse_formal_parameters(){
+    if(parse_variable_names() == S_ERROR) return S_ERROR;
+    token = scan();
+    if(token != TCOLON) return error("ERROR: expect \":\" next to variable names");
+    token = scan();
+    if(parse_type() == S_ERROR) return S_ERROR;
+    token = scan();
+    while(token == TSEMI){
+        token = scan();
+        if(parse_variable_names() == S_ERROR) return S_ERROR;
+        token = scan();
+        if(token != TCOLON) return error("ERROR: expect \":\" next to variable names");
+        token = scan();
+        if(parse_type() == S_ERROR) return S_ERROR;
+        token = scan();
+    }
+    if(token != TRPAREN) return error("ERROR: expect \")\" next to variable names");
+    return 0;
+}
+
+//#endregion

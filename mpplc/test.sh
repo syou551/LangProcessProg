fea1b5493515ce2af7f43da55574da57e1ce4fb8
syou551 <hiroto.0108si+@gmail.com>
2025-01-06 17:41:58 +0900

    add sample and update .gitignore

--- mpplc/test.sh
+++ mpplc/test.sh
@@ -4,9 +4,15 @@ filelist=$(ls ./sample)
 
 for file in $filelist
 do
-    ./cr ./sample/$file
+    ./mpplc ./sample/$file
 done
 
+filelist=$(ls /lpp_test/coverage/)
 
-./cr ./sample/1.txt
-./cr
+for file in $filelist
+do
+    ./mpplc /lpp_test/coverage/$file
+done
+
+./mpplc ./sample/1.txt
+./mpplc
